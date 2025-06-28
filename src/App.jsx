8b6099626f1b815ad4f744d222e6e14aa8e49815
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-06-28 12:54:33 +0530

    from childto parent props sent

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import Greeting from "./components/Greeting";
 //parent Component
 function App()
 {
-  const loggedInUser="Jaya";
+  // const loggedInUser={
+  //  name: "Jaya",
+  //  email:"jaya@gmail.com"
+  // }
+  //craete a carrer
+ function childToParent(name,email)
+ {
+  console.log("Function from parent to child");
+
+  console.log(name,email);
+ }
   return<>
-      <Greeting loggedInUser={loggedInUser}/>
+      <Greeting childToParent={childToParent}/>
 
      </>
 
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,14 @@
 function Greeting(props)
 {
    //child component of app 
+   
+    const name="jaya";
+     const email="jaya@gmail.com";
+     props.childToParent(name,email);
     return(
       <>
-      <h1>Hello ,{props.loggedInUser}</h1>
-      <p>This is my first component</p>
+      <h1>Hello ,{props.name}</h1>
+      <p>Your are loged in as {props.email}</p>
 
       </>
     )
