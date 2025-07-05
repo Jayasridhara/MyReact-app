71ab98a016bf3de93cf05a15228cc2256940db5d
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-07-05 12:43:39 +0530

    useeffect 3 types without[] and with [] and [state]

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [reactions,setReaction]=useState({likes:0,dislikes:0})
+  const [state,setState]=useState(0);
+  
+  useEffect(()=>{
+    fetch(`https://68457069fc51878754db59b6.mockapi.io/notes`)
+  .then(response=>response.json())
+  .then(data=>console.log(data))
 
-  console.log(reactions);
-  const handleLike=()=>{
-      setReaction({...reactions,likes:reactions.likes+1});
-  }
-  const handleDislike=()=>{
-    setReaction({...reactions,dislikes:reactions.dislikes+1});
-  }
-  return (
-    <>
-    <button onClick={handleLike}>Like {reactions.likes}</button>
-    <button onClick={handleDislike}>Dislike {reactions.dislikes}</button>
-    
-    </>
-    
+  },[state])
+
+  return(
+    <div>
+      <h1>App</h1>
+      <button onClick={()=>setState(state+1)}>
+      Re-Render
+      </button>
+    </div>
   )
 }
 
--- src/View.jsx
+++ src/View.jsx
@@ -2,34 +2,24 @@ import { useState } from "react";
 
 function View()
 {
-
-    const [count,setCount]=useState(0);
-
-
-    const handleIncrese=(plus)=>
-    {
-        setCount(count+1);
-        console.log("Increase",plus)
-    }
-    const handleDecrease=(minus)=>
-    {
-        setCount(count-1);
-        console.log("Increase",minus)
-    }
-    const handleReset=(zero)=>
-    {
-        setCount(0);
-        console.log("Reset",zero)
-    }
-    return (
-        <>
-            <h1>Count:{count}</h1>
-            <button onClick={()=>handleIncrese("plus")}>Increase</button>
-            <button onClick={()=>handleDecrease("minus")}>Decrease</button>
-            <button onClick={()=>handleReset("zero")}>Reset</button>
-
-        </>
-    )
+    
+ const [reactions,setReaction]=useState({likes:0,dislikes:0})
+ 
+   console.log(reactions);
+   const handleLike=()=>{
+       setReaction({...reactions,likes:reactions.likes+1});
+   }
+   const handleDislike=()=>{
+     setReaction({...reactions,dislikes:reactions.dislikes+1});
+   }
+   return (
+     <>
+     <button onClick={handleLike}>Like {reactions.likes}</button>
+     <button onClick={handleDislike}>Dislike {reactions.dislikes}</button>
+     
+     </>
+     
+   )
 }
 
 export default View;
