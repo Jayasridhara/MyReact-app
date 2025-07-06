6cd649dedbd3b3d155a5f7c8b56288aebc3f7082
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-07-06 12:33:12 +0530

    form controlled and uncomtrolled

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import Note from './components/Note';
+// import React, { useEffect, useState } from 'react'
+// import Note from './components/Note';
 
-function App() {
-  const [notes,setNote]=useState([]);
+// function App() {
+//   const [notes,setNote]=useState([]);
   
-  useEffect(()=>{
-    fetch(`https://68457069fc51878754db59b6.mockapi.io/notes`)
-  .then(response=>response.json())
-  .then(data=>setNote(data))
+//   useEffect(()=>{
+//     fetch(`https://68457069fc51878754db59b6.mockapi.io/notes`)
+//   .then(response=>response.json())
+//   .then(data=>setNote(data))
 
-  },[])
+//   },[])
 
-  console.log(notes);
-  return(
-    <div>
-      <h1>NoteApp</h1>
-      <ul>
-        {
-          notes.map((note,index)=>{
-           return <Note note={note} key={note.id}/>
-          })
+//   console.log(notes);
+//   return(
+//     <div>
+//       <h1>NoteApp</h1>
+//       <ul>
+//         {
+//           notes.map((note,index)=>{
+//            return <Note note={note} key={note.id}/>
+//           })
            
-        }
+//         }
        
-     </ul>
+//      </ul>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useEffect, useState } from 'react'
+
+//uncontrolled component in react ? beacause we are not using state to manage the form input
+//using use state is controlled component
+function App() {
+
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const handleLogin=(event)=>
+  {
+    event.preventDefault();
+   console.log(email,password)
+  }
+  useEffect(()=>
+  {
+    console.log(email,password)
+  },[email])
+  return (
+    <div>
+      <h1>login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <input type='text'name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='email..'></input>
+
+        </div>
+        <br/>
+        <div>
+          <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password..' name="password"></input>
+        </div>
+        <br/>
+        <button>Login</button>
+      </form>
 
     </div>
   )
