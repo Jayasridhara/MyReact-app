2859de8912364e9f6c3cfc896f2b4880cbb6c98e
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-07-13 11:37:19 +0530

    create router version 7 updatef

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-//create a router object
-const router=createBrowserRouter([
+const routes=[
 {
     path:'/',
     element:<h1>Hello World</h1>
+},
+{
+    path:'/about',
+    element:<h1>About Us</h1>
+},
+{
+    path:'/contactus',
+    element:<h1>Contact US</h1>
 }
-]);
+];
+//create a router object
+const router=createBrowserRouter(routes,{
+  future: {
+  v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
 function App() {
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router}
+    future={{
+        v7_startTransition: true,
+      }} />
   )
 }
 
